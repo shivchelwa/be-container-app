dd359121ca0f06453ed726ad900856e4a5db237c
Shivkumar Chelwa <shivkumar.chelwa@macys.com>
2019-01-04 09:00:00 -0500

    Added Rule and Virtual Rule Function

---
+++ JavaSrc/com/tibco/util/UtilFunctions.java
@@ -0,0 +1,26 @@
+package com.tibco.util;
+
+import java.util.UUID;
+
+import com.tibco.be.model.functions.BEFunction;
+import com.tibco.be.model.functions.BEPackage;
+import static com.tibco.be.model.functions.FunctionDomain.ACTION;
+
+@BEPackage(catalog = "Util", category = "Functions", synopsis = "Utility Functions")
+public class UtilFunctions {
+	
+	@BEFunction(name = "UUID", 
+			description="Generates a UUID", 
+			signature="String UUID()",
+			       freturn = @com.tibco.be.model.functions.FunctionParamDescriptor(name = "", type = "String", desc = "Returns a randomly generated UUID."),
+			params = {},
+			fndomain = {ACTION}, version="1.0",see="",cautions="none", example=" ")
+	public static String UUID() {
+		return UUID.randomUUID().toString();
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(UUID());
+	}
+
+}
